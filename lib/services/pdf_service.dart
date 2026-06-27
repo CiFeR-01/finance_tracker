0cecb1047ac515b7236ec3f6c264c59aa4dad8a6
junht <jungaytan0115@gmail.com>
2026-06-27 14:54:02 +0800

    pdf table header colours

--- lib/services/pdf_service.dart
+++ lib/services/pdf_service.dart
@@ -26,18 +26,9 @@ class PdfService {
           return pw.Container(
             alignment: pw.Alignment.centerRight,
             margin: const pw.EdgeInsets.only(top: 20),
-            child: pw.Row(
-              mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
-              children: [
-                pw.Text(
-                  'Generated on ${DateFormat('dd/MM/yyyy HH:mm').format(DateTime.now())}',
-                  style: pw.TextStyle(color: PdfColors.grey, fontSize: 10),
-                ),
-                pw.Text(
-                  'Page ${context.pageNumber} of ${context.pagesCount}',
-                  style: pw.TextStyle(color: PdfColors.grey, fontSize: 10),
-                ),
-              ],
+            child: pw.Text(
+              'Page ${context.pageNumber} of ${context.pagesCount}',
+              style: pw.TextStyle(color: PdfColors.grey, fontSize: 10),
             ),
           );
         },
@@ -193,18 +184,9 @@ class PdfService {
           return pw.Container(
             alignment: pw.Alignment.centerRight,
             margin: const pw.EdgeInsets.only(top: 20),
-            child: pw.Row(
-              mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
-              children: [
-                pw.Text(
-                  'Generated on ${DateFormat('dd/MM/yyyy HH:mm').format(DateTime.now())}',
-                  style: pw.TextStyle(color: PdfColors.grey, fontSize: 10),
-                ),
-                pw.Text(
-                  'Page ${context.pageNumber} of ${context.pagesCount}',
-                  style: pw.TextStyle(color: PdfColors.grey, fontSize: 10),
-                ),
-              ],
+            child: pw.Text(
+              'Page ${context.pageNumber} of ${context.pagesCount}',
+              style: pw.TextStyle(color: PdfColors.grey, fontSize: 10),
             ),
           );
         },
